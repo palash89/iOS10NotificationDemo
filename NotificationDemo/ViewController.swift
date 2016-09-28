409ccc58cc1e956d1f3eda6b747dd4cc50af85ba
Ravib <ravib@clariontechnologies.co.in>
2016-09-28 18:22:12 +0530

    First Commit

---
+++ NotificationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationDemo
+//
+//  Created by Yudiz Solutions Pvt.Ltd. on 26/09/16.
+//  Copyright © 2016 Yudiz Solutions Pvt.Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
